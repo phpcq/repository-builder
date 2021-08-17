a61d2b1b3fe5e6b31efa8fec9323dc287c1a0077
Christian Schiffler <c.schiffler@cyberspectrum.de>
2021-08-17 23:37:49 +0200

    Use CompoundRepository in RepositoryBuilder

--- src/RepositoryBuilder.php
+++ src/RepositoryBuilder.php
@@ -4,64 +4,22 @@ declare(strict_types=1);
 
 namespace Phpcq\RepositoryBuilder;
 
-use InvalidArgumentException;
-use Phpcq\RepositoryBuilder\SourceProvider\PluginVersionProvidingRepositoryInterface;
-use Phpcq\RepositoryBuilder\SourceProvider\SourceRepositoryInterface;
-use Phpcq\RepositoryBuilder\SourceProvider\ToolVersionEnrichingRepositoryInterface;
-use Phpcq\RepositoryBuilder\SourceProvider\ToolVersionProvidingRepositoryInterface;
+use Phpcq\RepositoryBuilder\SourceProvider\CompoundRepository;
 use Phpcq\RepositoryDefinition\Plugin\Plugin;
+use Phpcq\RepositoryDefinition\Plugin\PluginVersionInterface;
 use Phpcq\RepositoryDefinition\Repository;
 use Phpcq\RepositoryDefinition\Tool\Tool;
 use Phpcq\RepositoryDefinition\Tool\ToolVersionInterface;
 
 class RepositoryBuilder
 {
-    /**
-     * @var ToolVersionProvidingRepositoryInterface[]
-     */
-    private array $toolProviders = [];
-
-    /**
-     * @var ToolVersionEnrichingRepositoryInterface[]
-     */
-    private array $enrichingProviders = [];
-
-    /**
-     * @var PluginVersionProvidingRepositoryInterface[]
-     */
-    private array $pluginProviders = [];
+    private CompoundRepository $providers;
 
     private JsonRepositoryWriter $writer;
 
-    /**
-     * Create a new instance.
-     *
-     * @param SourceRepositoryInterface[] $providers
-     * @param JsonRepositoryWriter        $writer
-     */
-    public function __construct(array $providers, JsonRepositoryWriter $writer)
+    public function __construct(CompoundRepository $providers, JsonRepositoryWriter $writer)
     {
-        foreach ($providers as $provider) {
-            $supported = false;
-            if ($provider instanceof PluginVersionProvidingRepositoryInterface) {
-                $supported = true;
-                $this->pluginProviders[] = $provider;
-            } else {
-                if ($provider instanceof ToolVersionProvidingRepositoryInterface) {
-                    $supported = true;
-                    $this->toolProviders[] = $provider;
-                }
-                if ($provider instanceof ToolVersionEnrichingRepositoryInterface) {
-                    $supported = true;
-                    $this->enrichingProviders[] = $provider;
-                }
-            }
-
-            if (!$supported) {
-                throw new InvalidArgumentException('Unknown provider type ' . get_class($provider));
-            }
-        }
-
+        $this->providers = $providers;
         $this->writer = $writer;
     }
 
@@ -84,59 +42,41 @@ class RepositoryBuilder
 
     private function collectTools(Repository $repository): void
     {
-        foreach ($this->enrichingProviders as $enrichingProvider) {
-            if (!$enrichingProvider->isFresh()) {
-                $enrichingProvider->refresh();
+        foreach ($this->providers->getToolIterator() as $version) {
+            /** @var ToolVersionInterface $version */
+            $toolName = $version->getName();
+            if (!$repository->hasTool($toolName)) {
+                $repository->addTool(new Tool($toolName));
             }
-        }
+            $tool = $repository->getTool($toolName);
 
-        foreach ($this->toolProviders as $versionProvider) {
-            foreach ($versionProvider->getToolIterator() as $version) {
-                $toolName = $version->getName();
-                if (!$repository->hasTool($toolName)) {
-                    $repository->addTool(new Tool($toolName));
-                }
-                $tool = $repository->getTool($toolName);
-
-                if ($tool->has($version->getVersion())) {
-                    $other = $tool->getVersion($version->getVersion());
-                    $other->merge($version);
-                    continue;
-                }
-
-                $this->enrichVersion($version);
-                $tool->addVersion($version);
+            if ($tool->has($version->getVersion())) {
+                $other = $tool->getVersion($version->getVersion());
+                $other->merge($version);
+                continue;
             }
+
+            $tool->addVersion($version);
         }
     }
 
     private function collectPlugins(Repository $repository): void
     {
-        foreach ($this->pluginProviders as $versionProvider) {
-            foreach ($versionProvider->getToolIterator() as $version) {
-                $pluginName = $version->getName();
-                if (!$repository->hasPlugin($pluginName)) {
-                    $repository->addPlugin(new Plugin($pluginName));
-                }
-                $plugin = $repository->getPlugin($pluginName);
-
-                if ($plugin->has($version->getVersion())) {
-                    $other = $plugin->getVersion($version->getVersion());
-                    $other->merge($version);
-                    continue;
-                }
-
-                $plugin->addVersion($version);
+        foreach ($this->providers->getPluginIterator() as $version) {
+            /** @var PluginVersionInterface $version */
+            $pluginName = $version->getName();
+            if (!$repository->hasPlugin($pluginName)) {
+                $repository->addPlugin(new Plugin($pluginName));
             }
-        }
-    }
+            $plugin = $repository->getPlugin($pluginName);
 
-    private function enrichVersion(ToolVersionInterface $version): void
-    {
-        foreach ($this->enrichingProviders as $enrichingProvider) {
-            if ($enrichingProvider->supports($version)) {
-                $enrichingProvider->enrich($version);
+            if ($plugin->has($version->getVersion())) {
+                $other = $plugin->getVersion($version->getVersion());
+                $other->merge($version);
+                continue;
             }
+
+            $plugin->addVersion($version);
         }
     }
 }
