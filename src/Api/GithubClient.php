6d2a4a7013182b15d8afdaf08a57db456c491eef
Christian Schiffler <c.schiffler@cyberspectrum.de>
2021-08-18 13:40:12 +0200

    Remove superfluous "use"

--- src/Api/GithubClient.php
+++ src/Api/GithubClient.php
@@ -7,7 +7,6 @@ namespace Phpcq\RepositoryBuilder\Api;
 use Phpcq\RepositoryBuilder\Exception\DataNotAvailableException;
 use Phpcq\RepositoryBuilder\Util\StringUtil;
 use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Symfony\Component\HttpClient\Exception\ClientException;
